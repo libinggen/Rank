f7f936ff1a2fb72e5ecb7ff67cbc88943ba239e3
lison <lison1211@icloud.com>
2022-08-01 07:26:18 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  function StreamerList(props) {
+    const [streamerItems, setStreamerItems] = React.useState(0);
+    React.useEffect(() => {
+      const interval = setInterval(
+        () => setStreamerItems([
+          { key: "streamer01", value: Math.floor((Math.random() * 100) + 1) },
+          { key: "streamer02", value: Math.floor((Math.random() * 100) + 1) },
+          { key: "streamer03", value: Math.floor((Math.random() * 100) + 1) },
+          { key: "streamer04", value: Math.floor((Math.random() * 100) + 1) },
+          { key: "streamer05", value: Math.floor((Math.random() * 100) + 1) },
+          { key: "streamer06", value: Math.floor((Math.random() * 100) + 1) },
+          { key: "streamer07", value: Math.floor((Math.random() * 100) + 1) },
+          { key: "streamer08", value: Math.floor((Math.random() * 100) + 1) },
+          { key: "streamer09", value: Math.floor((Math.random() * 100) + 1) },
+          { key: "streamer10", value: Math.floor((Math.random() * 100) + 1) }
+        ]
+        .sort((a, b) => b.value - a.value)
+        .map((item) => <li key={item.key}>{item.key} : {item.value}</li>)),
+        1000
+      );
+
+      return () => {
+        clearInterval(interval);
+      };
+    }, []);
+
+    return (
+      <ul>{streamerItems}</ul>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StreamerList />
     </div>
   );
 }
